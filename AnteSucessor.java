d644336b6c029a0854a679dd166713bcaf6ea977
Clara Leticia <71187859+Clara-Farias@users.noreply.github.com>
2022-03-29 22:58:12 -0300

    Update AnteSucessor.java

--- AnteSucessor.java
+++ AnteSucessor.java
@@ -3,8 +3,9 @@
 package uni.edu.clara;
 import java.util.Scanner;
 public class AnteSucessor {
-    public static void main(String[] args){
-        try (Scanner in = new Scanner(System.in)) {
+   static Scanner in = new Scanner(System.in);
+   public static void main(String[] args){
+       
            
             System.out.println("Digite um Número: ");
             int num = in.nextInt();
@@ -12,8 +13,6 @@ public class AnteSucessor {
             int antecessor = num - 1;
             int sucessor = num + 1;
             // \n = quebra de linha
-            System.out.printf("O Antecessor de %d Será: %d \nE o seu Sucessor Será: %d \n" ,num,antecessor,sucessor);
-            
-        }
+            System.out.printf("O Antecessor de %d Será: %d \nE o seu Sucessor Será: %d \n" ,num,antecessor,sucessor); 
     }
 }
